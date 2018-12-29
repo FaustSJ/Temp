4960be262f5d9f4f587ef7c1d9c08b76706d0964
cloudera <cloudera@quickstart.cloudera>
2018-12-29 12:27:35 -0800

    PIG WORKS

--- difference.py
+++ difference.py
@@ -21,6 +21,10 @@ def latestYear(row):
 		count += 1
 	return latest_year_with_value;
 
+#double isn't a type in python, but float is.
+#when comparing this function's result to 0.0, Pig implicitly casts 0.0 as 
+#	a double, not a float. So pig can't check if this function's result
+#	is 0.0 because pig is looking for a 0.0 double, not a 0.0 float.
 @outputSchema("difference:double")      
 def difference(row):
 	first_value = 0.0
@@ -31,16 +35,17 @@ def difference(row):
 		if count > 53 :
 			data = str(k).split(",")
 			if data[0] != '(None':       
-				first_value = double(str(k)[1:-2])
+				first_value = float(str(k)[1:-2])
+				break
 		count += 1
 	count = 0
         for i,k in enumerate(row):
                 if count > 54 :
                         data = str(k).split(",")
                         if data[0] != '(None':
-                                last_value = double(str(k)[1:-2])
+                        	last_value = float(str(k)[1:-2])
                 count += 1
-        return latest_year_with_value;
-	if (first_value!=0) and (last_value!=0) and (first_value!=last_value) and (first_value<1.0) and (last_value>first_value):
+	if (first_value!=0.0) and (last_value!=0.0) and (first_value!=last_value) and (first_value<1.0) and (last_value>first_value):
 		return (last_value-first_value)
 	return 0.0
+
